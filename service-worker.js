3e2b7c7aae5a695acd5483edf337d7c524fde8d5
Ishan Sheth <contact@imsheth.com>
2016-05-24 17:50:14 +0530

    Changes sw sample

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-self.addEventListener('fetch', function(event) {
-  console.log('got a request');
+// self.addEventListener('fetch', function(event) {
+//   console.log('got a request');
 
-  var salutation = 'Hello, ';
-  var whom = decodeURIComponent(event.request.url.match(/\/([^/]*)$/)[1]);
-  var energyLevel = (whom === 'Cleveland') ? '!!!' : '!';
-  var version = '\n\n(Version 1)';
+//   var salutation = 'Hello, ';
+//   var whom = decodeURIComponent(event.request.url.match(/\/([^/]*)$/)[1]);
+//   var energyLevel = (whom === 'Cleveland') ? '!!!' : '!';
+//   var version = '\n\n(Version 1)';
 
-  var body = new Blob([salutation, whom, energyLevel, version]);
+//   var body = new Blob([salutation, whom, energyLevel, version]);
 
-  event.respondWith(new Response(body));
-});
+//   event.respondWith(new Response(body));
+// });
